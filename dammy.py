b881c3aa41d220c55b5644ec800f1577970a55f4
ukane praise <ukanepraiseg08@gmail.com>
2025-01-27 06:43:24 +0100

    Update dammy.py

--- dammy.py
+++ dammy.py
@@ -1,31 +1,22 @@
 greek_dict = {
-    "geia sas": "hello",
-    "antio": "goodbye",
-    "parakalo": "please",
-    "efharisto": "thank you",
-    "nai": "yes",
-    "ochi": "no",
-    "pos eisai?": "how are you?",
-    "kala": "fine",
-    "spiti": "home",
-    "filos": "friend",
-    "vasilias": "king",
-    "troo": "eat",
-    "vivlio": "book",
-    "edo": "here",
-    "ekei": "there",
-    "onoma": "name",
-    "nero": "water",
-    "scholeio": "school",
-    "agapi": "love",
-    "imera": "day"
+    'house': 'spiti',
+    'car': 'aftokinito',
+    'book': 'vivlio',
+    'water': 'nero',
+    'food': 'fagito',
+    'friend': 'filos',
+    'love': 'agapi',
+    'sun': 'ilios',
+    'moon': 'fengari',
+    'tree': 'dentro',
+    'sky': 'ouranos',
+    'flower': 'louloudi',
+    'mountain': 'vouno',
+    'river': 'potami',
+    'city': 'poli',
+    'dog': 'skylos',
+    'cat': 'gata',
+    'music': 'mousiki',
+    'family': 'oikogeneia',
+    'dream': 'oneiro'
 }
-
-def search_greek(word):
-    translation = greek_dict.get(word.lower())
-    return translation if translation else "Word not found in the Greek-to-English dictionary."
-
-search_term = input("Enter a Greek word (in English letters) to translate to English: ")
-
-result = search_greek(search_term)
-print(f"Translation: {result}")
